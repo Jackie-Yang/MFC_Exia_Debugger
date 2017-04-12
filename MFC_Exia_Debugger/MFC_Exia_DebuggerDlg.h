1450a8dd1fc1fb475daa319c47b36d38fd56cf18
Jackie <921622129@qq.com>
2017-04-12 15:41:02 +0800

    [feature]Init Git
    [what]初始化Git储存库
    [why]方便管理
    [how]VS2013添加

---
+++ MFC_Exia_Debugger/MFC_Exia_DebuggerDlg.h
@@ -0,0 +1,32 @@
+
+// MFC_Exia_DebuggerDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMFC_Exia_DebuggerDlg 对话框
+class CMFC_Exia_DebuggerDlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC_Exia_DebuggerDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MFC_EXIA_DEBUGGER_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
